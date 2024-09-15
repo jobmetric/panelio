63b509f4d3b85a5b10636cb15fceca11316ab868
majidmohammadian <majeedmohammadian@gmail.com>
2024-09-15 19:19:40 +0330

    added base language file

---
+++ lang/en/base.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Panelio Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Panelio for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "dashboard" => "Dashboard",
+
+];
