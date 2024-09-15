f09f184caeaac83ed60f4fcc87291da0317db536
majidmohammadian <majeedmohammadian@gmail.com>
2024-09-15 19:19:10 +0330

    add empty service

---
+++ src/Panelio.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace JobMetric\Panelio;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class Panelio
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * Create a new Translation instance.
+     *
+     * @param Application $app
+     *
+     * @return void
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+}
