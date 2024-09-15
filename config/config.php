6dc2ef874a57fd17b5e117dbfbdbab7d6f227125
majidmohammadian <majeedmohammadian@gmail.com>
2024-09-15 19:19:17 +0330

    add config file

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | Cache time for panelio
+    |
+    | - set zero for remove cache
+    | - set null for forever
+    |
+    | - unit: minutes
+    */
+
+    "cache_time" => env("PANELIO_CACHE_TIME", 0),
+
+];
