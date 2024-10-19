32b949905acd9d9aaff2973669f08908ffd0e2d3
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-19 22:13:03 +0330

    added function get current panel via url

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,19 @@
 <?php
+
+if (!function_exists('get_current_panel')) {
+    /**
+     * Get the current panel
+     *
+     * @return string|null
+     */
+    function get_current_panel(): ?string
+    {
+        $parts = explode('/', request()->path());
+
+        if (count($parts) > 1 && $parts[0] === 'p') {
+            return $parts[1];
+        }
+
+        return null;
+    }
+}
