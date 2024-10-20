84aced99016290720fc50a565457e3e12a34f79e
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-20 21:29:28 +0330

    remove change language script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
 $(document).ready(function () {
-    $('.change-language').on('click', function () {
-        let lang = $(this).data('lang');
-        $.ajax({
-            url: '/set-language',
-            type: 'POST',
-            data: {
-                _token: getCsrfToken(),
-                lang: lang
-            },
-            success: function (json) {
-                if(json.ok) {
-                    location.reload();
-                }
-            }
-        });
-    });
+    // Add Script
 });
