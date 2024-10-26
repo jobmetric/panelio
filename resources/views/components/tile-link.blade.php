cdf8d402d080606a68d691c86e8b3b84b08b312e
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-26 20:57:11 +0330

    change style tile link component

--- resources/views/components/tile-link.blade.php
+++ resources/views/components/tile-link.blade.php
@@ -1,6 +1,6 @@
 <div class="card h-lg-100">
     <a href="{{ $link }}" class="card-body w-100 text-center">
         {{ $slot }}
-        <span class="fw-semibold fs-3 text-gray-400">{{ $name }}</span>
+        <span class="fw-semibold fs-5 pt-5 text-gray-400 d-block">{{ $name }}</span>
     </a>
 </div>
