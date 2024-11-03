fcdf96ffa28627f2e450e24b748695e6540600d7
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-03 22:32:06 +0330

    added some language keys

--- lang/en/base.php
+++ lang/en/base.php
@@ -13,125 +13,126 @@ return [
     */
 
     "exceptions" => [
-        "panel_not_found" => "Panel :panel not found.",
-        "section_in_panel_not_found" => "Section :section in Panel :panel not found.",
-        "menu_in_section_panel_not_found" => "Menu :menu_name in Section :section in Panel :panel not found.",
-        "button_not_found" => "Button :button not found.",
+        "panel_not_found" => "Panel ':panel' not found.",
+        "section_in_panel_not_found" => "Section ':section' in Panel :panel not found.",
+        "menu_in_section_panel_not_found" => "Menu ':menu_name' in Section ':section' in Panel ':panel' not found.",
+        "button_not_found" => "Button ':button' not found.",
+        "alert_type_not_found" => "Alert type ':type' not found.",
     ],
 
-    'section' => [
-        'dashboard' => [
-            'name' => 'Dashboard',
-            'title' => 'Special Reports',
+    "section" => [
+        "dashboard" => [
+            "name" => "Dashboard",
+            "title" => "Special Reports",
         ],
-        'panels' => [
-            'name' => 'Panels',
-            'title' => 'Management Panels',
+        "panels" => [
+            "name" => "Panels",
+            "title" => "Management Panels",
         ],
-        'notification' => [
-            'title' => 'Notification',
-            'report' => ':number new report',
-            'all' => 'View All'
+        "notification" => [
+            "title" => "Notification",
+            "report" => ":number new report",
+            "all" => "View All"
         ],
-        'profile' => [
-            'title' => 'Profile',
-            'languages' => 'Languages',
-            'setting' => 'Setting',
-            'logout' => 'Logout',
-        ],
-    ],
-
-    'mode' => [
-        'light' => 'Light',
-        'dark' => 'Dark',
-        'system' => 'System',
-    ],
-
-    'show_website' => 'Show Website',
-
-    'button' => [
-        'operation' => 'Operation',
-        'select_option' => 'Choose an option',
-        'save' => 'Save',
-        'save_close' => 'Save & Close',
-        'save_new' => 'Save & New',
-        'cancel' => 'Cancel',
-        'add' => 'Add',
-        'export' => 'Export',
-        'import' => 'Import',
-        'status' => [
-            'enable' => 'Enable',
-            'disable' => 'Disable',
-        ],
-        'block' => 'Block',
-        'unblock' => 'Unblock',
-        'bulk' => 'Bulk Action',
-        'bulk_title' => 'Bulk Actions',
-        'setting' => 'Setting',
-        'setting_title' => 'Screen Options',
-        'help' => 'Help',
-
-        'delete' => 'Delete',
-        'recycle' => 'Recycle',
-        'edit' => 'Edit',
-        'realized' => 'Realized',
-    ],
-
-    'dashboard' => 'Dashboard',
-
-    'component' => [
-        'list_view' => [
-            'title' => 'list :name',
-            'search' => 'Search',
-            'filter' => 'Filter',
+        "profile" => [
+            "title" => "Profile",
+            "languages" => "Languages",
+            "setting" => "Setting",
+            "logout" => "Logout",
         ],
     ],
 
-    'message' => [
-        'status' => [
-            'enable' => 'Item :count status changed to active.',
-            'disable' => 'Item :count status changed to inactive.',
+    "mode" => [
+        "light" => "Light",
+        "dark" => "Dark",
+        "system" => "System",
+    ],
+
+    "show_website" => "Show Website",
+
+    "button" => [
+        "operation" => "Operation",
+        "select_option" => "Choose an option",
+        "save" => "Save",
+        "save_close" => "Save & Close",
+        "save_new" => "Save & New",
+        "cancel" => "Cancel",
+        "add" => "Add",
+        "export" => "Export",
+        "import" => "Import",
+        "status" => [
+            "enable" => "Enable",
+            "disable" => "Disable",
+        ],
+        "block" => "Block",
+        "unblock" => "Unblock",
+        "bulk" => "Bulk Action",
+        "bulk_title" => "Bulk Actions",
+        "setting" => "Setting",
+        "setting_title" => "Screen Options",
+        "help" => "Help",
+
+        "delete" => "Delete",
+        "recycle" => "Recycle",
+        "edit" => "Edit",
+        "realized" => "Realized",
+    ],
+
+    "dashboard" => "Dashboard",
+
+    "component" => [
+        "list_view" => [
+            "title" => "list :name",
+            "search" => "Search",
+            "filter" => "Filter",
         ],
     ],
 
-    'modal' => [
-        'import' => [
-            'title' => 'Import File',
-            'description' => 'Choose the file format you want to import.',
-            'formats' => 'Allowed formats: :formats',
-            'submit' => 'Import',
-            'route_error' => 'Import route not found.',
+    "message" => [
+        "status" => [
+            "enable" => "Item :count status changed to active.",
+            "disable" => "Item :count status changed to inactive.",
         ],
-        'export' => [
-            'title' => 'Export File',
-            'description' => 'Choose the file format you want to export.',
-            'submit' => 'Export',
-            'route_error' => 'Export route not found.',
+    ],
+
+    "modal" => [
+        "import" => [
+            "title" => "Import File",
+            "description" => "Choose the file format you want to import.",
+            "formats" => "Allowed formats: :formats",
+            "submit" => "Import",
+            "route_error" => "Import route not found.",
         ],
-        'import_export_options' => [
-            'json' => [
-                'name' => 'JSON',
-                'description' => 'JavaScript Object Notation',
+        "export" => [
+            "title" => "Export File",
+            "description" => "Choose the file format you want to export.",
+            "submit" => "Export",
+            "route_error" => "Export route not found.",
+        ],
+        "import_export_options" => [
+            "json" => [
+                "name" => "JSON",
+                "description" => "JavaScript Object Notation",
             ],
-            'xml' => [
-                'name' => 'XML',
-                'description' => 'Extensible Markup Language',
+            "xml" => [
+                "name" => "XML",
+                "description" => "Extensible Markup Language",
             ],
-            'excel' => [
-                'name' => 'Excel',
-                'description' => 'Microsoft Excel',
+            "excel" => [
+                "name" => "Excel",
+                "description" => "Microsoft Excel",
             ],
-            'word' => [
-                'name' => 'Word',
-                'description' => 'Microsoft Word',
+            "word" => [
+                "name" => "Word",
+                "description" => "Microsoft Word",
             ],
-            'csv' => [
-                'name' => 'CSV',
-                'description' => 'Comma Separated Values',
+            "csv" => [
+                "name" => "CSV",
+                "description" => "Comma Separated Values",
             ],
-            'pdf' => [
-                'name' => 'PDF',
-                'description' => 'Portable Document Format',
+            "pdf" => [
+                "name" => "PDF",
+                "description" => "Portable Document Format",
             ],
         ],
     ],
--- lang/fa/base.php
+++ lang/fa/base.php
@@ -13,127 +13,128 @@ return [
     */
 
     "exceptions" => [
-        "panel_not_found" => "پنل :panel موجود نیست.",
-        "section_in_panel_not_found" => "بخش :section در پنل :panel موجود نیست.",
-        "menu_in_section_panel_not_found" => "منو :menu_name در بخش :section در پنل :panel موجود نیست.",
-        "button_not_found" => "دکمه :button موجود نیست.",
+        "panel_not_found" => "پنل ':panel' موجود نیست.",
+        "section_in_panel_not_found" => "بخش ':section' در پنل ':panel' موجود نیست.",
+        "menu_in_section_panel_not_found" => "منو ':menu_name' در بخش ':section' در پنل ':panel' موجود نیست.",
+        "button_not_found" => "دکمه ':button' موجود نیست.",
+        "alert_type_not_found" => "نوع هشدار ':type' موجود نیست.",
     ],
 
-    'section' => [
-        'dashboard' => [
-            'name' => 'داشبورد',
-            'title' => 'گزارشات ویژه',
+    "section" => [
+        "dashboard" => [
+            "name" => "داشبورد",
+            "title" => "گزارشات ویژه",
         ],
-        'panels' => [
-            'name' => 'پنل ها',
-            'title' => 'پنل های مدیریتی',
+        "panels" => [
+            "name" => "پنل ها",
+            "title" => "پنل های مدیریتی",
         ],
-        'notification' => [
-            'title' => 'اطلاع رسانی',
-            'report' => ':number گزارش جدید',
-            'all' => 'مشاهده همه'
+        "notification" => [
+            "title" => "اطلاع رسانی",
+            "report" => ":number گزارش جدید",
+            "all" => "مشاهده همه"
         ],
-        'profile' => [
-            'title' => 'پروفایل',
-            'languages' => 'زبان ها',
-            'setting' => 'تنظیمات',
-            'logout' => 'خروج',
-        ],
-    ],
-
-    'mode' => [
-        'light' => 'روشن',
-        'dark' => 'تاریک',
-        'system' => 'سیستم',
-    ],
-
-    'show_website' => 'نمایش وب سایت',
-
-    'button' => [
-        'operation' => 'عملیات',
-        'select_option' => 'یک گزینه انتخاب کنید',
-        'save' => 'ذخیره',
-        'save_close' => 'ذخیره و بستن',
-        'save_new' => 'ذخیره و جدید',
-        'cancel' => 'انصراف',
-        'add' => 'افزودن',
-        'export' => 'خروجی',
-        'import' => 'بارگذاری',
-        'status' => [
-            'enable' => 'فعال',
-            'disable' => 'غیر فعال',
-        ],
-        'block' => 'محروم',
-        'unblock' => 'عدم محرومیت',
-        'bulk' => 'کار دسته جمعی',
-        'bulk_title' => 'کار دسته جمعی',
-        'setting' => 'تنظیمات',
-        'setting_title' => 'تنظیمات نمایش',
-        'help' => 'راهنما',
-        'help_title' => 'راهنمای صفحه',
-
-        'delete' => 'حذف',
-        'recycle' => 'بازیافت',
-        'edit' => 'ویرایش',
-        'realized' => 'متوجه شدم',
-    ],
-
-    'dashboard' => 'داشبورد',
-
-    'component' => [
-        'list_view' => [
-            'title' => 'لیست :name',
-            'search' => 'جستجو',
-            'filter' => 'فیلتر',
-            'delete_filters' => 'حذف فیلترها',
+        "profile" => [
+            "title" => "پروفایل",
+            "languages" => "زبان ها",
+            "setting" => "تنظیمات",
+            "logout" => "خروج",
         ],
     ],
 
-    'message' => [
-        'status' => [
-            'enable' => 'تعداد :count مورد فعال شد.',
-            'disable' => 'تعداد :count مورد غیر فعال شد.',
+    "mode" => [
+        "light" => "روشن",
+        "dark" => "تاریک",
+        "system" => "سیستم",
+    ],
+
+    "show_website" => "نمایش وب سایت",
+
+    "button" => [
+        "operation" => "عملیات",
+        "select_option" => "یک گزینه انتخاب کنید",
+        "save" => "ذخیره",
+        "save_close" => "ذخیره و بستن",
+        "save_new" => "ذخیره و جدید",
+        "cancel" => "انصراف",
+        "add" => "افزودن",
+        "export" => "خروجی",
+        "import" => "بارگذاری",
+        "status" => [
+            "enable" => "فعال",
+            "disable" => "غیر فعال",
+        ],
+        "block" => "محروم",
+        "unblock" => "عدم محرومیت",
+        "bulk" => "کار دسته جمعی",
+        "bulk_title" => "کار دسته جمعی",
+        "setting" => "تنظیمات",
+        "setting_title" => "تنظیمات نمایش",
+        "help" => "راهنما",
+        "help_title" => "راهنمای صفحه",
+
+        "delete" => "حذف",
+        "recycle" => "بازیافت",
+        "edit" => "ویرایش",
+        "realized" => "متوجه شدم",
+    ],
+
+    "dashboard" => "داشبورد",
+
+    "component" => [
+        "list_view" => [
+            "title" => "لیست :name",
+            "search" => "جستجو",
+            "filter" => "فیلتر",
+            "delete_filters" => "حذف فیلترها",
         ],
     ],
 
-    'modal' => [
-        'import' => [
-            'title' => 'بارگذاری داده با فایل',
-            'description' => 'لطفا فایل خود را انتخاب کنید.',
-            'formats' => 'فرمت های مجاز: :formats',
-            'submit' => 'بارگذاری',
-            'route_error' => 'مسیر ورودی یافت نشد.',
+    "message" => [
+        "status" => [
+            "enable" => "تعداد :count مورد فعال شد.",
+            "disable" => "تعداد :count مورد غیر فعال شد.",
         ],
-        'export' => [
-            'title' => 'خروجی فایل',
-            'description' => 'لطفا یک فرمت خروجی انتخاب کنید.',
-            'submit' => 'خروجی',
-            'route_error' => 'مسیر خروجی یافت نشد.',
+    ],
+
+    "modal" => [
+        "import" => [
+            "title" => "بارگذاری داده با فایل",
+            "description" => "لطفا فایل خود را انتخاب کنید.",
+            "formats" => "فرمت های مجاز: :formats",
+            "submit" => "بارگذاری",
+            "route_error" => "مسیر ورودی یافت نشد.",
         ],
-        'import_export_options' => [
-            'json' => [
-                'name' => 'JSON',
-                'description' => 'خروجی فایل JSON',
+        "export" => [
+            "title" => "خروجی فایل",
+            "description" => "لطفا یک فرمت خروجی انتخاب کنید.",
+            "submit" => "خروجی",
+            "route_error" => "مسیر خروجی یافت نشد.",
+        ],
+        "import_export_options" => [
+            "json" => [
+                "name" => "JSON",
+                "description" => "خروجی فایل JSON",
             ],
-            'xml' => [
-                'name' => 'XML',
-                'description' => 'خروجی فایل XML',
+            "xml" => [
+                "name" => "XML",
+                "description" => "خروجی فایل XML",
             ],
-            'excel' => [
-                'name' => 'Excel',
-                'description' => 'خروجی فایل Excel',
+            "excel" => [
+                "name" => "Excel",
+                "description" => "خروجی فایل Excel",
             ],
-            'word' => [
-                'name' => 'Word',
-                'description' => 'خروجی فایل Word',
+            "word" => [
+                "name" => "Word",
+                "description" => "خروجی فایل Word",
             ],
-            'csv' => [
-                'name' => 'CSV',
-                'description' => 'خروجی فایل CSV',
+            "csv" => [
+                "name" => "CSV",
+                "description" => "خروجی فایل CSV",
             ],
-            'pdf' => [
-                'name' => 'PDF',
-                'description' => 'خروجی فایل PDF',
+            "pdf" => [
+                "name" => "PDF",
+                "description" => "خروجی فایل PDF",
             ],
         ],
     ],
