4d54c5041b59b6f9ba52a09c5e83d12d1f63307d
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-11 18:51:08 +0330

    added switch show filter and write table-responsive class

--- resources/views/components/list-view.blade.php
+++ resources/views/components/list-view.blade.php
@@ -10,30 +10,34 @@
             <span class="text-gray-800">{{ trans('panelio::base.component.list_view.title', ['name' => $name]) }}</span>
         </h3>
         <div class="card-toolbar">
-            <div id="list-btn-filter-box" class="d-none">
-                <button type="button" class="btn btn-sm btn-light-facebook me-3" id="list-btn-search">
-                    <i class="la la-search fs-2 position-absolute"></i>
-                    <span class="d-none d-md-inline ps-9">{{ trans('panelio::base.component.list_view.search') }}</span>
+            @if(isset($filter) && $filter !== '')
+                <div id="list-btn-filter-box" class="d-none">
+                    <button type="button" class="btn btn-sm btn-light-facebook me-3" id="list-btn-search">
+                        <i class="la la-search fs-2 position-absolute"></i>
+                        <span class="d-none d-md-inline ps-9">{{ trans('panelio::base.component.list_view.search') }}</span>
+                    </button>
+                    <button type="button" class="btn btn-sm btn-icon btn-light-facebook me-3 px-3" id="list-btn-delete-filter" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ trans('panelio::base.component.list_view.delete_filters') }}">
+                        <i class="la la-remove fs-2 p-0"></i>
+                    </button>
+                </div>
+                <button type="button" class="btn btn-sm btn-light-info btn-active-light-info" id="list-btn-filter">
+                    <i class="la la-filter fs-2 position-absolute"></i>
+                    <span class="d-none d-md-inline ps-9">{{ trans('panelio::base.component.list_view.filter') }}</span>
                 </button>
-                <button type="button" class="btn btn-sm btn-icon btn-light-facebook me-3 px-3" id="list-btn-delete-filter" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ trans('panelio::base.component.list_view.delete_filters') }}">
-                    <i class="la la-remove fs-2 p-0"></i>
-                </button>
-            </div>
-            <button type="button" class="btn btn-sm btn-light-info btn-active-light-info" id="list-btn-filter">
-                <i class="la la-filter fs-2 position-absolute"></i>
-                <span class="d-none d-md-inline ps-9">{{ trans('panelio::base.component.list_view.filter') }}</span>
-            </button>
+            @endif
         </div>
     </div>
     <div class="card-body py-5">
-        <div class="border-bottom mb-5 d-none" id="list-filter-box">
-            <div class="row align-items-center">
-                {{ $filter ?? '' }}
+        @if(isset($filter) && $filter !== '')
+            <div class="border-bottom mb-5 d-none" id="list-filter-box">
+                <div class="row align-items-center">
+                    {!! $filter !!}
+                </div>
             </div>
-        </div>
+        @endif
         <form method="post" action="{{ $action }}" id="list-form" data-export="{{ $exportAction }}" data-import="{{ $importAction }}">
             @csrf
-            <div class="dataTables_wrapper">
+            <div class="table-responsive">
                 <table class="table table-bordered table-striped table-hover table-check dataTable" id="datatable">
                     {{ $slot }}
                 </table>
